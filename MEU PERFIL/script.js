5fe53eadf354d03a6a1f5c0db25b5c7b51c92396
luizfelipe17 <luizfeliperabelo17@gmail.com>
2025-04-08 16:35:46 -0300

    Add files via upload

---
+++ MEU PERFIL/script.js
@@ -0,0 +1,7 @@
+document.getElementById('minhaImagem').addEventListener('click', function(){
+
+    var audio = new Audio("mp3/bombardino-crocodilo.mp3");
+    audio.play();
+    audio.volume(1);
+
+});
